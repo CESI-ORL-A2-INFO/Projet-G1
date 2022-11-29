33b55a249ac78df3dc8ef3b9d3e4bc7d1483a259
Balisto91 <115796843+Balisto91@users.noreply.github.com>
2022-11-29 15:36:28 +0100

    Cad.h

---
+++ Cad.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace NS_Comp_Data {
+	ref class Cad {
+	public:
+		System::String^ sSql;
+		System::String^ sCnx;
+		System::Data::SqlClient::SqlConnection^ oCnx;
+		System::Data::SqlClient::SqlCommand^ oCmd;
+		System::Data::DataSet^ oDs;
+		System::Data::DataSet^ getRows(System::String^, System::String^);
+		void actionRows(System::String^);
+	};
+}
