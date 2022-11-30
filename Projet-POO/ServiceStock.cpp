cde468b6c4e6d46f86e560f9c28ddf013e830030
Aly-Rida MAHJOUB <alyrida.mahjoub@viacesi.fr>
2022-11-30 11:47:08 +0100

    Service Stock Done

--- Projet-POO/ServiceStock.cpp
+++ Projet-POO/ServiceStock.cpp
@@ -1,1 +1,62 @@
 #include "ServiceStock.h"
+
+
+void ServiceStock::addCata(System::String^ designation, System::String^ nature_article, System::String^ couleur, int seuil_approvisionnement, int id_tva, float prixHT) {
+	System::String^ sql;
+
+	this->mapCata->setDesignation(designation);										// On associe la données entrée a la désignation d'un article
+	this->mapCata->setNatureArticle(nature_article);								// On associe la données entrée a la nature d'un article
+	this->mapCata->setCouleur(couleur);												// On associe la données entrée a la couleur d'un article
+	this->mapCata->setSeuilApprovisionnement(seuil_approvisionnement);				// On associe la données entrée au seuil d'approvisionnement
+	this->mapCata->setIDTVA(id_tva);												// On associe la données entrée a l'id de la tva du produit
+	this->mapCata->setPrixHT(prixHT);												// On associe la données entrée au prix hors taxe du produit
+
+	sql = this->mapCata->Insert();													// On insere les données dans la base de données 
+
+	this->cataCad->actionRows(sql);
+}
+
+
+void ServiceStock::updateCata(int id_catalogue, System::String^ designation, System::String^ nature_article, System::String^ couleur, int seuil_approvisionnement, int id_tva, float prixHT) {
+	System::String^ sql;
+
+	this->mapCata->setIdCatalogue(id_catalogue);									// On associe la données entrée a l'ID du catalogue qu'on veut modifier
+	this->mapCata->setDesignation(designation);										// On associe la données entrée a la désignation d'un article qu'on veut modifier
+	this->mapCata->setNatureArticle(nature_article);								// On associe la données entrée a la nature d'un article qu'on veut modifier
+	this->mapCata->setCouleur(couleur);												// On associe la données entrée a la couleur d'un article qu'on veut modifier
+	this->mapCata->setSeuilApprovisionnement(seuil_approvisionnement);				// On associe la données entrée au seuil d'approvisionnement qu'on veut modifier
+	this->mapCata->setIDTVA(id_tva);												// On associe la données entrée a l'id de la tva du produit qu'on veut modifier
+	this->mapCata->setPrixHT(prixHT);												// On associe la données entrée au prix hors taxe du produit qu'on veut modifier
+
+	sql = this->mapCata->Update();													// On met a jour les données dans la base de données 
+
+	this->cataCad->actionRows(sql);
+}
+
+
+void ServiceStock::delCata(int id_catalogue) {
+	System::String^ sql;
+
+	this->mapCata->setIdCatalogue(id_catalogue);									// On associe la données entrée a l'ID du catalogue qu'on veut supprimer
+
+	sql = this->mapCata->Delete();													// On supprime les données dans la base de données 
+
+	this->cataCad->actionRows(sql);
+}
+
+
+System::Data::DataSet^ ServiceStock::selectAllCata(System::String^ nom_table) {
+	System::String^ sql;
+
+	sql = this->mapCata->Select();
+
+	return this->cataCad->getRows(sql, nom_table);
+}
+
+System::Data::DataSet^ ServiceStock::selectCata(System::String^ nom_table, int id_catalogue) {
+	System::String^ sql;
+
+	sql = this->mapCata->Select();
+
+	return this->cataCad->getRows(sql, nom_table);
+}
--- Projet-POO/ServiceStock.h
+++ Projet-POO/ServiceStock.h
@@ -15,8 +15,8 @@ private:
 	alimente MapAlimente;
 
 public:
-	void addCata(int, System::String^, System::String^, int, int, float, float, float, float);				// Fonction qui permet d'ajouter un catalogue 
-	void updateCata(int, int, System::String^, System::String^, int, int, float, float, float, float);		// Fonction qui permet de mettre a jour un catalogue
+	void addCata(System::String^, System::String^, System::String^ , int, int, float);						// Fonction qui permet d'ajouter un catalogue 
+	void updateCata(int, System::String^, System::String^, System::String^, int, int, float);				// Fonction qui permet de mettre a jour un catalogue
 	void delCata(int);																						// Fonction qui permet de supprimer un catalogue
 	DataSet selectAllCata(System::String^);																	// Fonction qui permet de selectionner tout les catalogue
 	DataSet selectCata(System::String^, int);																// Fonction qui permet de selectionner un catalogue
