95a3b35e93f3c0d7154b4bb60b7a3e76e8c14e7d
Matthieu PUGNETTI <matthieu.pugnetti@viacesi.fr>
2022-11-30 11:31:55 +0100

    Service commande

---
+++ ServiceClient.cpp
@@ -0,0 +1,10 @@
+#include "ServiceClient.h"
+
+
+
+void addClient(System::String^, System::String^, System::String^, System::String^, int, int);
+
+
+
+
+void udpdateClient(int, System::String^, System::String^, System::String^, System::String^, int, int);
